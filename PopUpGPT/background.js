dbf6f808f3ae0689974f64479a1788066e08de76
Issan0511 <issanakatsuka0511@gmail.com>
2025-01-07 22:12:03 +0900

    edit background js

--- PopUpGPT/background.js
+++ PopUpGPT/background.js
@@ -1,30 +1,29 @@
 chrome.runtime.onInstalled.addListener(() => {
-    // 「SearchGPTで『選択テキスト』を検索」メニュー
-    chrome.contextMenus.create({
-      id: "searchGPT",
-      title: 'Search "%s" in SearchGPT',
-      contexts: ["selection"]
-    });
-    
-    // // 「SearchGPTで『選択テキスト』とは何か聞く」メニュー
-    // chrome.contextMenus.create({
-    //   id: "askWhatIs",
-    //   title: 'SearchGPTで「%s」とは何か聞く',
-    //   contexts: ["selection"]
-    // });
+  console.log("PopUpGPT installed");
+
+  chrome.contextMenus.create({
+    id: "searchGPT",
+    title: 'Search "%s" in SearchGPT',
+    contexts: ["selection"]
   });
-  
-  chrome.contextMenus.onClicked.addListener((info, tab) => {
-    const query = info.selectionText;
-    
-    if (info.menuItemId === "searchGPT" && query) {
-      const url = `https://chatgpt.com/?q=${encodeURIComponent(query)}&hints=search&ref=ext&temporary-chat=true`;
-      chrome.tabs.create({ url: url });
-    }
-    
-    if (info.menuItemId === "askWhatIs" && query) {
-      const questionUrl = `https://chatgpt.com/?q=${encodeURIComponent(query)}+とは何かについて分かりやすく簡潔に教えて下さい&hints=search&ref=ext&temporary-chat=true`;
-      chrome.tabs.create({ url: questionUrl });
-    }
-  });
-  
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  const query = info.selectionText;
+
+  if (info.menuItemId === "searchGPT" && query) {
+    const url = `https://chatgpt.com/?q=${encodeURIComponent(query)}&hints=search&ref=ext&temporary-chat=true`;
+    chrome.tabs.create({ url: url });
+  }
+});
+
+// ショートカットキーのリスナーを追加
+chrome.commands.onCommand.addListener((command) => {
+  console.log(`Command received: ${command}`);
+
+  if (command === "open-chatgpt") {
+    const url = "https://chatgpt.com/";
+    chrome.tabs.create({ url: url });
+    console.log(`Navigating to ${url}`);
+  }
+});
